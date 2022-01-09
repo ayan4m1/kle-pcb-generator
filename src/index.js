10d5efd596ae517085b8e64c989a789defa0ea27
ayan4m1 <andrew@bulletlogic.com>
2022-01-09 13:37:59 -0500

    continue developing script to place components

--- src/index.js
+++ src/index.js
@@ -1,47 +1,75 @@
 import { basename, join, dirname } from 'path';
 import { Command } from 'commander';
+import { writeFile } from 'fs/promises';
 
-import { getBoardScript, getSchematicScript } from './modules/eagle.js';
+import {
+  diodes,
+  getBoardScript,
+  getSchematicScript,
+  keySwitches
+} from './modules/eagle.js';
 import { parseKleFile } from './modules/kle.js';
 import { getLogger } from './modules/logging.js';
 
 const log = getLogger('app');
 const program = new Command();
 
+program.version('0.1.0');
+
 program
-  .version('0.1.0')
+  .command('convert', {
+    isDefault: true
+  })
+  .description('converts a KLE JSON into EAGLE scripts')
   .arguments('<input>')
-  .option('-d, --diode <name>', 'Diode part name')
-  .option('-k, --key-switch <name>', 'Keyswitch part name')
+  .option('-d, --diode <name>', 'Diode part name', 'tht')
+  .option('-k, --key-switch <name>', 'Keyswitch part name', 'alps')
   .action(async (input, options) => {
-    let diode = 'DIODE-DO-35';
+    try {
+      const diode = diodes.get(options.diode);
+      const keySwitch = keySwitches.get(options.keySwitch);
 
-    if (options.diode) {
-      diode = options.diode.toUpperCase();
+      if (!diode) {
+        return log.error('Invalid diode specified!');
+      }
+
+      if (!keySwitch) {
+        return log.error('Invalid key switch specified!');
+      }
+
+      log.info(`Using diode ${diode} and switch ${keySwitch}`);
+
+      const fileName = basename(input, '.json');
+      const outputSchematic = join(dirname(input), `${fileName}-schematic.scr`);
+      const outputBoard = join(dirname(input), `${fileName}-board.scr`);
+
+      log.info(`Parsing ${input}`);
+
+      const keyboard = await parseKleFile(input);
+
+      const schematicScript = getSchematicScript(keyboard, diode, keySwitch);
+      const boardScript = getBoardScript(keyboard);
+
+      await writeFile(outputSchematic, schematicScript);
+      await writeFile(outputBoard, boardScript);
+    } catch (error) {
+      log.error(error.message);
+      log.error(error.stack);
     }
+  });
 
-    let keySwitch = 'KEYSWITCH-PLAIN-ALPSMX';
-
-    if (options.keySwitch) {
-      keySwitch = options.keySwitch.toUpperCase();
+program
+  .command('parts')
+  .description('list available parts')
+  .action(() => {
+    log.info('Diodes:');
+    for (const [name, value] of diodes.entries()) {
+      log.info(`\t${name} - ${value}`);
     }
-
-    const fileName = basename(input, '.json');
-    const outputSchematic = join(dirname(input), `${fileName}.sch`);
-    const outputBoard = join(dirname(input), `${fileName}.brd`);
-    const keyboard = await parseKleFile(input);
-
-    log.info(input);
-    log.info(outputSchematic);
-    log.info(outputBoard);
-    log.info(diode);
-    log.info(keySwitch);
-
-    const schematicScript = getSchematicScript(keyboard, diode, keySwitch);
-    const boardScript = getBoardScript(keyboard);
-
-    console.dir(schematicScript);
-    console.dir(boardScript);
+    log.info('Switches:');
+    for (const [name, value] of keySwitches.entries()) {
+      log.info(`\t${name} - KEYSWITCH-PLAIN-${value}`);
+    }
   });
 
 program.parse(process.argv);
--- src/modules/eagle.js
+++ src/modules/eagle.js
@@ -9,6 +9,26 @@ keyMap.set('/', 'SLASH');
 keyMap.set('\\', 'SOLIDUS');
 keyMap.set('[', 'LBRACKET');
 keyMap.set(']', 'RBRACKET');
+keyMap.set(';', 'SEMI');
+keyMap.set("'", 'QUOTE');
+keyMap.set('CAPS LOCK', 'CAPSLK');
+
+const keyWidths = [1, 2, 4, 6, 6.25, 7];
+
+export const diodes = new Map();
+
+diodes.set('tht', 'DIODE-DO-35');
+diodes.set('smd', 'DIODE-SOD-123');
+
+export const keySwitches = new Map();
+
+keySwitches.set('alps', 'ALPS');
+keySwitches.set('alpsMx', 'ALPSMX');
+keySwitches.set('choc', 'CHOC');
+keySwitches.set('chocX', 'CHOCX');
+keySwitches.set('mx', 'MX');
+keySwitches.set('mxHs', 'MXHS');
+keySwitches.set('mxHsPcb', 'MXHSPCB');
 
 const getLabel = (key) => {
   if (!key.labels.length) {
@@ -29,6 +49,10 @@ const getLabel = (key) => {
 };
 
 export const getSchematicScript = (keyboard, diode, keySwitch) => {
+  const diodeOffset = [0.1, 0.7];
+  const switchOffset = [0.8, 1.7];
+  const diodeSize = [0.2, 0.4];
+
   const preamble = `
 GRID ON;
 GRID IN 0.1 1;
@@ -41,6 +65,7 @@ WINDOW FIT;
 `;
 
   const labelMap = new Map();
+  const lastPositions = new Map();
 
   const main = keyboard.keys
     .map((key) => {
@@ -55,17 +80,75 @@ WINDOW FIT;
         labelMap.set(label, index);
       }
 
+      const { x, y, width } = key;
+      const row = Math.floor(y) + 1;
+      const nets = [];
+
+      let keyWidth = width;
+
+      if (!keyWidths.includes(width)) {
+        keyWidth = Math.floor(width);
+      }
+
+      const switchPos = [x * switchOffset[0], y * -switchOffset[1]];
+
+      if (width > 1) {
+        switchPos[0] += (switchOffset[0] * (width - 1)) / 2;
+      }
+
+      const diodePos = [
+        switchPos[0] + diodeOffset[0],
+        switchPos[1] + diodeOffset[1]
+      ];
+
+      if (lastPositions.has(row)) {
+        const rowPositions = lastPositions.get(row);
+        const lastDiodePos = rowPositions[rowPositions.length - 1];
+
+        nets.push(
+          `NET ROW${row} (${diodePos[0].toFixed(2)} ${(
+            diodePos[1] +
+            diodeSize[1] / 2
+          ).toFixed(2)}) (${lastDiodePos[0].toFixed(2)} ${(
+            diodePos[1] +
+            diodeSize[1] / 2
+          ).toFixed(2)})`
+        );
+        rowPositions.push(diodePos);
+      } else {
+        lastPositions.set(row, [diodePos]);
+      }
+
+      nets.push(
+        `NET (${diodePos[0].toFixed(2)} ${(
+          diodePos[1] -
+          diodeSize[1] / 2
+        ).toFixed(2)}) (${diodePos[0].toFixed(2)} ${(
+          diodePos[1] -
+          0.1 -
+          diodeSize[1] / 2
+        ).toFixed(2)})`
+      );
+
       return `
-ADD ${keySwitch}-${key.width}U ${label} (0 0);
-ADD ${diode} D${label} R90 (0 0);
+ADD KEYSWITCH-PLAIN-${keySwitch}-${keyWidth}U ${label} (${switchPos[0].toFixed(
+        2
+      )} ${switchPos[1].toFixed(2)});
+ADD ${diode} D${label} R90 (${diodePos[0].toFixed(2)} ${diodePos[1].toFixed(
+        2
+      )});
+${nets.join('\n')}
 `;
     })
-    .join('\n');
+    .join('');
 
   return `${preamble}${main}${footer}`;
 };
 
 export const getBoardScript = (keyboard) => {
+  // const diodeOffset = [];
+  // const switchOffset = [19.05, 19.05];
+
   const preamble = `
 GRID ON;
 GRID MM 1 10;
@@ -83,7 +166,7 @@ ROTATE R270 D${label};
 MOVE D${label} (0 0);
 `;
     })
-    .join('\n');
+    .join('');
 
   const footer = `
 RATSNEST;
